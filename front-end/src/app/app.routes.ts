80b9a7a19826bd3a82eee88d6833458c4d45de21
sebahomsi <45207797+sebahomsi@users.noreply.github.com>
2021-11-08 12:19:22 -0300

    se agregar el routes

---
+++ front-end/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoute: Routes = []
+
+const appRoutes: Routes = [
+];
+
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
